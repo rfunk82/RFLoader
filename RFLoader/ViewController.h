677bbe286a5ebc0f01fb006b22af35a2dee727aa
rfunk82 <rfunk82@gmail.com>
2013-12-03 15:25:47 -0500

    Initial Commit

---
+++ RFLoader/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RFLoader
+//
+//  Created by Ricardo Funk on 12/3/13.
+//  Copyright (c) 2013 Ricardo Funk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ RFLoaderTests/RFLoaderTests.m
@@ -0,0 +1,34 @@
+//
+//  RFLoaderTests.m
+//  RFLoaderTests
+//
+//  Created by Ricardo Funk on 12/3/13.
+//  Copyright (c) 2013 Ricardo Funk. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RFLoaderTests : XCTestCase
+
+@end
+
+@implementation RFLoaderTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
